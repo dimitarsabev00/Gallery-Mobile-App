826567324c2cbe5a30f4c65bc17397cff1e13287
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-10-28 20:04:59 +0200

    Added Carousel Component & Use It In App.tsx File

--- App.tsx
+++ App.tsx
@@ -1,23 +1,42 @@
-import { FlatList, Image, SafeAreaView, StyleSheet } from "react-native";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  useWindowDimensions,
+  View,
+} from "react-native";
 
 import { photos } from "./helpers/photos";
+import { Carousel } from "./components";
 
 export default function App() {
+  const { height, width } = useWindowDimensions();
+
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={photos}
-        numColumns={4}
-        contentContainerStyle={{ gap: 2 }}
-        columnWrapperStyle={{ gap: 2 }}
-        renderItem={({ item }) => (
-          <Image
-            source={item.image}
-            style={{ width: `${100 / 4}%`, aspectRatio: 1 }}
-          />
-        )}
-      />
-    </SafeAreaView>
+    <ScrollView style={styles.container}>
+      <View style={{ height: height / 2 }}>
+        <FlatList
+          data={photos}
+          numColumns={4}
+          contentContainerStyle={{ gap: 2 }}
+          columnWrapperStyle={{ gap: 2 }}
+          scrollEnabled={false}
+          inverted
+          renderItem={({ item }) => (
+            <Image
+              source={item.image}
+              style={{ width: `${100 / 4}%`, aspectRatio: 1 }}
+            />
+          )}
+        />
+      </View>
+
+      <Carousel title="Albums" photos={photos.slice(0, 6)} />
+      <Carousel title="People" photos={photos.slice(3, 6)} />
+      <Carousel title="Featured" photos={photos.slice(6, 10)} />
+    </ScrollView>
   );
 }
 
