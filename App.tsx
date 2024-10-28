a237eedb98eafe739ac576bd49828632c84ea59b
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-10-28 17:43:54 +0200

    Initialize React Native Application With Expo + TypeScript

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Gallery Mobile App</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
