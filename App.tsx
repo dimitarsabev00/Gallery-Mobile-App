d6652f6dea8cc1b688d900f4e3e569c926c18675
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-10-28 20:13:39 +0200

    Added headerCarousel Logic In App.tsx File

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import {
   FlatList,
   Image,
-  SafeAreaView,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
   ScrollView,
   StyleSheet,
   useWindowDimensions,
@@ -10,14 +11,37 @@ import {
 
 import { photos } from "./helpers/photos";
 import { Carousel } from "./components";
+import { useState } from "react";
 
 export default function App() {
   const { height, width } = useWindowDimensions();
+  const [headerCarouselPage, setHeaderCarouselPage] = useState(0);
+
+  const onHeaderCarouselScroll = (
+    e: NativeSyntheticEvent<NativeScrollEvent>
+  ) => {
+    const curPage = Math.max(
+      0,
+      Math.floor((e.nativeEvent.contentOffset.x + width / 2) / width)
+    );
+    if (curPage !== headerCarouselPage) {
+      setHeaderCarouselPage(curPage);
+    }
+  };
 
   return (
     <ScrollView style={styles.container}>
-      <View style={{ height: height / 2 }}>
+      <ScrollView
+        horizontal
+        style={{ height: height / 2 }}
+        snapToInterval={width}
+        snapToAlignment="start"
+        decelerationRate="fast"
+        showsHorizontalScrollIndicator={false}
+        onScroll={onHeaderCarouselScroll}
+      >
         <FlatList
+          style={{ width }}
           data={photos}
           numColumns={4}
           contentContainerStyle={{ gap: 2 }}
@@ -31,8 +55,42 @@ export default function App() {
             />
           )}
         />
-      </View>
+        <Image
+          source={photos[0].image}
+          style={{ width, height: "100%" }}
+          resizeMode="cover"
+        />
+        <Image
+          source={photos[10].image}
+          style={{ width, height: "100%" }}
+          resizeMode="cover"
+        />
+      </ScrollView>
 
+      <View
+        style={{
+          padding: 10,
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "row",
+          gap: 5,
+        }}
+      >
+        {Array(3)
+          .fill(0)
+          .map((item, index) => (
+            <View
+              key={index}
+              style={{
+                width: index === headerCarouselPage ? 10 : 8,
+                aspectRatio: 1,
+                backgroundColor:
+                  index === headerCarouselPage ? "black" : "gray",
+                borderRadius: 5,
+              }}
+            />
+          ))}
+      </View>
       <Carousel title="Albums" photos={photos.slice(0, 6)} />
       <Carousel title="People" photos={photos.slice(3, 6)} />
       <Carousel title="Featured" photos={photos.slice(6, 10)} />
